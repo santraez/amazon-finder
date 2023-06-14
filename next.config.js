8619d987a2e12464cca3b1b705f91b6e1a8cc162
santraez <santraez@gmail.com>
2023-06-13 20:27:36 -0500

    input and list

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'm.media-amazon.com'
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as cheerio from 'cheerio';
+import axios from 'axios';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const { url } = req.query;
+  const { data } = await axios.get(url);  
+  const $ = cheerio.load(data);
+  const $title = $('#productTitle').text();
+  const $price = $('#corePrice_feature_div .a-offscreen').text();
+  const $about = $('#feature-bullets ul li span').text();
+  const $image = $('#landingImage').attr('src');
+  res.status(200).json({ title: $title, price: $price, about: $about, image: $image })
 }
